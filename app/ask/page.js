34f200647d73b1ff887e099a52bc21b003d59aae
Anish Raj Pandey <anishrajpandey@Anishs-MacBook-Air.local>
2024-11-03 09:34:37 -0600

    prod fix

--- app/ask/page.js
+++ app/ask/page.js
@@ -4,18 +4,18 @@ import Prompt from '../_components/Prompt'
 
 import ChatThread from '../_components/ChatThread'
 import Navbar from '../_components/Navbar'
-const page = () => {
+const Page = () => {
   const [toggle,setToggle]=useState(false);
   return (
     <>
     <Navbar/>
 
     <main className='px-12'> 
-<ChatThread trigger={toggle } />
+< ChatThread trigger={toggle } />
 <Prompt onToggle={setToggle} toggle={toggle } />
     </main>
     </>
   )
 }
 
-export default page;
+export default Page;
